9f4e4cfe13df82ea33642fb6c569546ca279dade
Marcos Eduardo <bacalhau_1995@hotmail.com>
2022-05-15 20:19:10 -0300

    add axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,11 +21,11 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { Header } from "../../components/Header";
 import { useQuery } from "react-query";
+import { api } from "../../services/api";
 
 export default function UserList() {
   const { data, isLoading, error, isFetching } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
+    const { data } = await api.get("/users");
 
     const users = data.users.map(user => {
       return {
